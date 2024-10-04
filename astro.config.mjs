1005cb4965b63f743cb538208edbd6f12d1d8818
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2024-10-04 10:41:21 +0200

    Initial commit with Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
-// Astro Configuration
+// Check if the environment is production (i.e., GitHub Pages)
+const isGitHubPages = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+  site: isGitHubPages ? 'https://jurilorenz.github.io/maxwapp' : 'http://localhost:3000',
+  base: isGitHubPages ? '/maxwapp/' : '/',  // Use the base path only for production
+  build: {
+    outDir: 'dist',
+  },
   integrations: [
-    tailwind(), // Keep Tailwind for your styles
+    tailwind(),
     icon({
       include: {
         fa: ["github", "linkedin"], 
